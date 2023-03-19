043f8fd4fe3fa9d03556dfd959378747705fcb29
Ziarotun Ni'mah <128268310+Zia070494@users.noreply.github.com>
2023-03-19 10:08:23 +0700

    Add files via upload

---
+++ latihan1/contoh5.py.py
@@ -0,0 +1,7 @@
+class PesawatTerbang:
+    def __init__(self, maskapai, tujuan):
+        self.maskapai = maskapai
+        self.tujuan = tujuan
+
+    def info(self):
+        print(f"Maskapai: {self.maskapai}\nTujuan: {self.tujuan}")
